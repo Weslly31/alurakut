2440591da2446dc764572d2f2726bfecef65429b
weslley <weslley.reis@mail.uft.edu.br>
2021-07-14 01:04:39 -0300

    inicio alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *{
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    font-family: sans-serif;
   }
+  body {
+    box-sizing: border-box;
+    background-color: #d9e6f6;
+  }
+  #_next {
+    display: flex;
+    min-height: 100 vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
